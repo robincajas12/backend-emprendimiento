4ab8333491a34eb3d7a66feb2063b52673e64e16
robincajas12 <robincajas2020@gmail.com>
2025-11-26 20:44:01 -0500

    Started project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("io.freefair.lombok") version "9.1.0"
 }
 
 group = "com.project007"
@@ -13,6 +14,25 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // CDI (Weld SE)
+    implementation("org.jboss.weld.se:weld-se-core:6.0.3.Final")
+    // Opcional: Jandex (útil si haces scanning)
+    implementation("io.smallrye:jandex:3.2.3")
+    implementation("org.projectlombok:lombok:1.18.42")
+
+    implementation("jakarta.persistence:jakarta.persistence-api:3.2.0")
+
+    // https://mvnrepository.com/artifact/org.hibernate.orm/hibernate-core
+    implementation("org.hibernate.orm:hibernate-core:7.2.0.CR3")
+
+
+    implementation("org.postgresql:postgresql:42.7.8")
+
+}
+sourceSets{
+    main{
+        output.setResourcesDir(file("${buildDir}/classes/java/main"))
+    }
 }
 
 tasks.test {
--- src/main/java/com/project007/Main.java
+++ src/main/java/com/project007/Main.java
@@ -1,17 +1,23 @@
 package com.project007;
 
+
+import com.project007.db.IUserService;
+import com.project007.db.User;
+import jakarta.enterprise.inject.se.SeContainer;
+import jakarta.enterprise.inject.se.SeContainerInitializer;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try(SeContainer container = SeContainerInitializer.newInstance().initialize())
+        {
+          IUserService us =  container.select(IUserService.class).get();
+          us.create(User.builder().name("robin").build());
+          us.selectAll().forEach(System.out::println);
+        }catch (Exception e)
+        {
+            e.printStackTrace();
         }
     }
 }
